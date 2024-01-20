6a49b2cdded8ba9448cdc3e51cddd88b37a1e644
murad <murad@gmail.com>
2024-01-20 18:41:20 +0100

    ee

--- scr.js
+++ scr.js
@@ -4,16 +4,20 @@ let b = document.getElementById('num2');
 function topla() {
     let netice = +a.value + +b.value;
     document.getElementById('ayaz').innerHTML = netice;
+    document.getElementById('murad').innerHTML = netice.toFixed(2);
 }
 function cix() {
     let netice = +a.value - +b.value;
     document.getElementById('ayaz').innerHTML = netice;
+    document.getElementById('murad').innerHTML = netice.toFixed(2);
 }
 function vur() {
     let netice = +a.value * +b.value;
     document.getElementById('ayaz').innerHTML = netice;
+    document.getElementById('murad').innerHTML = netice.toFixed(2);
 }
 function bol() {
     let netice = +a.value / +b.value;
     document.getElementById('ayaz').innerHTML = netice;
+    document.getElementById('murad').innerHTML = netice.toFixed(2);
 }
